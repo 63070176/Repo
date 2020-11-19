295151517aaa411d530e78ba4e0d2550b899bca7
Diskzxc <73645806+Diskzxc@users.noreply.github.com>
2020-11-19 15:28:43 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -1,28 +1,41 @@
 <html>
 <head>
-<title>ITF Lab</title>
+<title>ITF Lab Show information</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<style>
+body {
+  background-color: rgba(172, 207, 246, 1);
+}
+</style>
 </head>
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'lab-itf01-11-13.mysql.database.azure.com', 'dis092550@lab-itf01-11-13', '44751Dis', 'itflab', 3306);
+mysqli_real_connect($conn, 'tanadon.mysql.database.azure.com', 'tanadonparosin@tanadon', 'Nexus404', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+<center>
+<table class="table table-hover table-bordered table-center" style="width:800px">
+  <tr class="table-primary" >
+    <th>Name</th>
+    <th>Comment</th>
+    <th>Link</th>
   </tr>
+<center\>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-  <tr>
+  <tr class="table-primary">
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
@@ -34,5 +47,8 @@ while($Result = mysqli_fetch_array($res))
 <?php
 mysqli_close($conn);
 ?>
+<a href="https://itflab2020.azurewebsites.net/form.html" class="btn btn-info">add</a>
+<a href="https://itflab2020.azurewebsites.net/form_edit.html" class="btn btn-warning">edit</a>
+<a href="https://itflab2020.azurewebsites.net/form_delete.html" class="btn btn-danger">delete</a>
 </body>
 </html>
